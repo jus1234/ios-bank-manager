ff1d13aa3887ec70b23edb7df03f74031e5b1fd7
Hoi.Kwon <teaho177@gmail.com>
2024-02-07 17:40:39 +0900

    stop3

--- BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank.swift
@@ -3,54 +3,40 @@ import Foundation
 final class Bank {
     private let customerCountRange: ClosedRange<Int> = 10...15
     private var totalCustomer: Int
-    private var customerQueue: LinkedListQueue<Customer>
-    private var loanCustomerQueue: LinkedListQueue<Customer>
-    private var depositManagerQueue: DispatchSemaphore
-    private var loanManagerQueue: DispatchSemaphore
+    private let customerQueue: LinkedListQueue<Customer>
+    private let loanCustomerQueue: LinkedListQueue<Customer>
+    private var depositSemaphore: DispatchSemaphore
+    private var loanSemaphore: DispatchSemaphore
     
     init() {
         self.totalCustomer = 0
         self.customerQueue = LinkedListQueue<Customer>()
         self.loanCustomerQueue = LinkedListQueue<Customer>()
-        self.depositManagerQueue = DispatchSemaphore(value: 2)
-        self.loanManagerQueue = DispatchSemaphore(value: 1)
+        self.depositSemaphore = DispatchSemaphore(value: 2)
+        self.loanSemaphore = DispatchSemaphore(value: 1)
     }
 }
 
-
 extension Bank {
-    
     func process() {
-        print("process")
-        let openTime: Date = Date()
-            Message.menu.printMessage()
-            Message.input.printMessage()
-            let selectedMenu = validate()
-            switch selectedMenu {
-            case .wrongInput:
-                Message.wrongInput.printMessage()
-                process()
-            case .open:
-                makeCustomerQueue()
-                openBank()
-            case .exit:
-                break
-            }
-    }
-    
-    private func validate() -> Menu {
-        
-        guard let userInput = readLine() else {
-            return .wrongInput
+        Message.menu.printMessage()
+        Message.input.printMessage()
+        guard let userInput = readLine() else { return }
+        switch Menu(input: userInput) {
+        case .wrongInput:
+            Message.wrongInput.printMessage()
+            process()
+        case .open:
+            makeCustomerQueue()
+            openBank()
+        case .exit:
+            break
         }
-        return Menu(input: userInput)
     }
     
     private func makeCustomerQueue() {
-        let totalCustomer = Int.random(in: customerCountRange) 
+        let totalCustomer = Int.random(in: customerCountRange)
         self.totalCustomer = totalCustomer
-        print("\(totalCustomer)")
-        
         for number in 1...totalCustomer {
             let task: Task = Bool.random() ? .deposit : .loan
             let customer = Customer(number: number, task: task)
@@ -65,33 +51,25 @@ extension Bank {
     private func openBank() {
         let openTime: Date = Date()
         let dispatchGroup = DispatchGroup()
-        print("Opening bank")
-        processQueue(loanCustomerQueue, semaphore: loanManagerQueue, dispatchGroup: dispatchGroup, queueType: "loan")
-        processQueue(customerQueue, semaphore: depositManagerQueue, dispatchGroup: dispatchGroup, queueType: "deposit")
-        
-        dispatchGroup.notify(queue: .global()) {
-            print("notiy started!!💊")
-//            self.closeBank(openTime)
-        }
+        processQueue(loanCustomerQueue, semaphore: loanSemaphore, dispatchGroup: dispatchGroup, queueType: "loan")
+        processQueue(customerQueue, semaphore: depositSemaphore, dispatchGroup: dispatchGroup, queueType: "deposit")
         dispatchGroup.wait()
         closeBank(openTime)
     }
+    
     private func processQueue(_ queue: LinkedListQueue<Customer>, semaphore: DispatchSemaphore, dispatchGroup: DispatchGroup,queueType: String) {
         while let customer = queue.dequeue() {
             dispatchGroup.enter()
-            print("\(queueType) Queue - Customer \(customer.number) dequeue started")
             semaphore.wait()
             DispatchQueue.global().async {
                 self.processCustomer(customer) {
-                    print("\(queueType) Queue - Customer \(customer.number) processing completed")
                     semaphore.signal()
                     dispatchGroup.leave()
                 }
             }
         }
     }
-    
-    
+
     private func processCustomer(_ customer: Customer, completion: @escaping () -> Void) {
         let manager = BankManager()
         manager.deal(with: customer, isLastCustomer: totalCustomer == 0) { (manager, task, isLastCustomer) in
@@ -100,10 +78,8 @@ extension Bank {
     }
     
     private func closeBank(_ openTime: Date) {
-        print("closeBank호출")
         Message.close(customerCount: totalCustomer, time: Date().timeIntervalSince(openTime)).printMessage()
         totalCustomer = 0
         process()
     }
 }
-
